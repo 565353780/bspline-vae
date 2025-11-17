eab8351ba46668cb1abc98e351ad8cde36ef9b12
chli <chli@chlideMacBook-Pro.local>
2025-11-17 15:27:31 +0800

    add setup bash files

---
+++ dev_setup.sh
@@ -0,0 +1,9 @@
+cd ..
+git clone git@github.com:565353780/bspline-surface.git
+git clone git@github.com:565353780/vecset-vae.git
+
+cd bspline-surface
+./dev_setup.sh
+
+cd ../vecset-vae
+./dev_setup.sh
---
+++ setup.sh
@@ -0,0 +1,9 @@
+cd ..
+git clone https://github.com/565353780/bspline-surface.git
+git clone https://github.com/565353780/vecset-vae.git
+
+cd bspline-surface
+./setup.sh
+
+cd ../vecset-vae
+./setup.sh
